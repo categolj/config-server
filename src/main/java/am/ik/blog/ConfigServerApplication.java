9e731b45a5e668a732b6b4ad47a626518fc8ae2b
making <makingx@gmail.com>
2017-04-15 03:59:22 +0900

    Update for local dev

--- src/main/java/am/ik/blog/ConfigServerApplication.java
+++ src/main/java/am/ik/blog/ConfigServerApplication.java
@@ -1,12 +1,19 @@
 package am.ik.blog;
 
+import org.apache.catalina.filters.RequestDumperFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
+
+	@Bean
+	RequestDumperFilter requestDumperFilter() {
+		return new RequestDumperFilter();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
